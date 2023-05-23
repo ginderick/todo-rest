12e2ef19572308e6fc04608303338269e84724bc
Gin <ginderick@gmail.com>
2023-05-23 20:10:55 +0800

    add addTodo service for creating a todo item

--- src/services/todos.ts
+++ src/services/todos.ts
@@ -7,4 +7,16 @@ export default class TodosService {
     const todos = prisma.todo.findMany({});
     return todos;
   }
+
+  public async addTodo(todo: any) {
+    const todoItem = prisma.todo.create({
+      data: {
+        name: todo.name,
+        description: todo.description,
+        remarks: todo.remarks,
+        dateCreated: todo.dateCreated,
+      },
+    });
+    return todoItem;
+  }
 }
