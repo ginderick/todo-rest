df67449700244398a1be943e3d0e67b31eda4c14
Gin <ginderick@gmail.com>
2023-05-24 10:04:33 +0800

    add UpdateTodoSchema and TodoParamSchema for validation

--- src/schema/TodoSchema.ts
+++ src/schema/TodoSchema.ts
@@ -6,4 +6,16 @@ export const TodoSchema = z.object({
   remarks: z.string(),
 });
 
+export const UpdateTodoSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  remarks: z.string().optional(),
+});
+
+export const TodoParamSchema = z.object({
+  id: z.coerce.number(),
+});
+
 export type TodoSchema = z.infer<typeof TodoSchema>;
+export type UpdateTodoSchema = z.infer<typeof UpdateTodoSchema>;
+export type TodoParamSchema = z.infer<typeof TodoParamSchema>;
