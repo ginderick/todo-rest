3c473ffa1f2d5ab7d5e2239f2d66de147888616a
Gin <ginderick@gmail.com>
2023-05-24 13:10:00 +0800

    add todoList object for testing

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -18,3 +18,30 @@ export const loginWrongPasswordPayload = {
   username: 'ginderick',
   password: 'Test123',
 };
+
+export const todoList = [
+  {
+    id: 1,
+    name: 'todo1',
+    description: 'todo1 description',
+    remarks: 'todo1 remarks',
+    dateCreated: new Date(),
+    dateCompleted: null,
+  },
+  {
+    id: 2,
+    name: 'todo2',
+    description: 'todo2 description',
+    remarks: 'todo2 remarks',
+    dateCreated: new Date(),
+    dateCompleted: null,
+  },
+  {
+    id: 3,
+    name: 'todo3',
+    description: 'todo3 description',
+    remarks: 'todo3 remarks',
+    dateCreated: new Date(),
+    dateCompleted: null,
+  },
+];
