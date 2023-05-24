3f7152b030a624d2b540fef6b4e3a227ba276743
Gin <ginderick@gmail.com>
2023-05-24 11:40:03 +0800

    add a check if token is blacklisted in jwt strategy

--- src/api/strategies/jwtStrategy.ts
+++ src/api/strategies/jwtStrategy.ts
@@ -2,21 +2,34 @@ import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt';
 import prisma from '../../../prisma';
 import {Payload} from '../../types';
 import config from '../../config';
+import {Request} from 'express';
 
 const jwtOptions = {
   secretOrKey: config.token.privateJWTKey,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  passReqToCallback: true,
 };
 
-export const jwtStrategy = new JwtStrategy(jwtOptions, async (payload: Payload, done: any) => {
-  try {
-    const username = payload.sub;
-    const user = await prisma.user.findUnique({where: {username: username}});
-    if (!user) {
-      return done(null, false);
+export const jwtStrategy = new JwtStrategy(
+  jwtOptions,
+  async (req: Request, payload: Payload, done: any) => {
+    try {
+      const username = payload.sub;
+      const user = await prisma.user.findUnique({where: {username: username}});
+      const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req)!;
+
+      const blacklistToken = await prisma.blacklistedToken.findUnique({
+        where: {
+          token: token,
+        },
+      });
+
+      if (!user || blacklistToken) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
     }
-    return done(null, user);
-  } catch (error) {
-    return done(error);
   }
-});
+);
