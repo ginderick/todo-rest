3d5ad220febed11d7496d0a34f30dff650510e19
Gin <ginderick@gmail.com>
2023-05-24 12:47:42 +0800

    update user payload and username

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,10 +1,15 @@
 export const user = {
   id: 1,
-  username: 'ginderick6',
+  username: 'ginderick',
   hashed_password: '$2b$10$yHtyfSVWK3IcBNmNw.Glx.NzRitAPrpk5SXZjlioMIUcuGlSKwPo2',
 };
 
 export const loginPayload = {
+  username: 'ginderick',
+  password: 'Test123!',
+};
+
+export const userPayload = {
   username: 'ginderick',
   password: 'Test123!',
 };
